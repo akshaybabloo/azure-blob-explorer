255ca4b675bd0d8de87812344ea3a0afc89b4031
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-06-17 00:07:00 +1200

    methods init

--- src/main/java/com/gollahalli/azure/Utils.java
+++ src/main/java/com/gollahalli/azure/Utils.java
@@ -1,4 +1,57 @@
 package com.gollahalli.azure;
 
+import com.microsoft.azure.storage.CloudStorageAccount;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+
+/**
+ * Utilities for {@link AzureBlobUpload} and {@link AzureBlobDownload}.
+ */
 public class Utils {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    private CloudStorageAccount account;
+
+    /**
+     * @param account
+     *      Requires {@link CloudStorageAccount} details.
+     */
+    public Utils(CloudStorageAccount account) {
+        this.account = account;
+    }
+
+    /**
+     * List all the containers in the account provided.
+     *
+     * @return A list of containers.
+     */
+    public List<String> listContainers(){
+        return null;
+    }
+
+    /**
+     * Creates a container.
+     *
+     * @param containerName
+     *      Name of the container.
+     * @return
+     *      A URI of the container created.
+     */
+    public String createContainer(String containerName){
+        return null;
+    }
+
+    /**
+     * Checks if a containers exists with the name given.
+     *
+     * @param containerName
+     *      Name of the container.
+     * @return <code>false</code> if container does not exist, <code>true</code> otherwise.
+     */
+    public boolean conatinerExists(String containerName){
+        return false;
+    }
 }
