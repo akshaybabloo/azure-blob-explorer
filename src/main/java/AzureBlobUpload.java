e1acc5d757e90bbc664d422de487437c39cd3de0
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-06-16 23:09:08 +1200

    code restructured and methods updated

--- src/main/java/AzureBlobUpload.java
+++ src/main/java/AzureBlobUpload.java
@@ -1,18 +1,76 @@
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageCredentialsAccountAndKey;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.URISyntaxException;
+
+/**
+ * Implements uploading files to Azure containers.
+ */
 public class AzureBlobUpload {
 
-    private String accountKey;
     private String accountName;
+    private String accountKey;
+    private String containerName;
+    private boolean useHttps;
 
-    public AzureBlobUpload(String accountKey, String accountName) {
-        this.accountKey = accountKey;
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * @param accountName   Account name from your <strong>Access Keys</strong>.
+     * @param accountKey    Account key from your <strong>Access Keys</strong>.
+     * @param containerName Container name you want to upload or create.
+     * @param useHttps      <code>true</code> to use HTTPS to connect to the storage service endpoints;
+     *                      otherwise, <code>false</code>. Defaults to <code>true</code>.
+     */
+    public AzureBlobUpload(String accountName, String accountKey, String containerName, boolean useHttps) {
         this.accountName = accountName;
+        this.accountKey = accountKey;
+        this.useHttps = useHttps;
+        this.containerName = containerName;
     }
 
-    public String upload(String path){
+    /**
+     * @param accountName   Account name from your <strong>Access Keys</strong>.
+     * @param accountKey    Account key from your <strong>Access Keys</strong>.
+     * @param containerName Container name you want to upload or create.
+     */
+    public AzureBlobUpload(String accountName, String accountKey, String containerName) {
+        this(accountName, accountKey, containerName, true);
+    }
+
+
+    /**
+     * Upload a single file to Azure blob.
+     *
+     * @param path An absolute path to the file.
+     * @return URL of the uploaded file.
+     * @throws URISyntaxException If an invalid account name is provided.
+     */
+    public String upload(String path) throws URISyntaxException {
+
+        StorageCredentialsAccountAndKey accountAndKey = new StorageCredentialsAccountAndKey(this.accountName, this.accountKey);
+        CloudStorageAccount account = new CloudStorageAccount(accountAndKey, this.useHttps);
+
+        CloudBlobClient cloudBlobClient = null;
+        CloudBlobContainer cloudBlobContainer = null;
+
+        cloudBlobClient = account.createCloudBlobClient();
+
+        return "";
 
     }
 
-    public String recursiveUpload(String path){
 
+    /**
+     * @param folderPath Absolute path to a folder.
+     * @return URL of the uploaded location.
+     */
+    public String recursiveUpload(String folderPath) {
+        return "";
     }
+
 }
