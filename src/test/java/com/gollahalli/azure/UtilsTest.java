06adf40ca81cb6c1957e6d9e121462c38341e036
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-06-18 11:16:09 +1200

    test init

--- src/test/java/com/gollahalli/azure/UtilsTest.java
+++ src/test/java/com/gollahalli/azure/UtilsTest.java
@@ -1,5 +1,30 @@
+package com.gollahalli.azure;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilsTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void listContainers() {
+    }
+
+    @Test
+    void createContainer() {
+    }
+
+    @Test
+    void conatinerExists() {
+    }
 }
