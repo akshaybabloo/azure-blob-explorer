e1b0963d4843ba451c7affe3da2ce062d6dd00d8
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-06-18 12:19:06 +1200

    file formatted

--- src/main/java/com/gollahalli/azure/Utils.java
+++ src/main/java/com/gollahalli/azure/Utils.java
@@ -17,8 +17,7 @@ public class Utils {
     private CloudStorageAccount account;
 
     /**
-     * @param account
-     *      Requires {@link CloudStorageAccount} details.
+     * @param account Requires {@link CloudStorageAccount} details.
      */
     public Utils(CloudStorageAccount account) {
         this.account = account;
@@ -29,53 +28,47 @@ public class Utils {
      *
      * @return A list of containers.
      */
-    public List<String> listContainers(){
+    public List<String> listContainers() {
         return null;
     }
 
     /**
      * Creates a container.
      *
-     * @param containerName
-     *      Name of the container.
-     * @return
-     *      A URI of the container created.
+     * @param containerName Name of the container.
+     * @return A URI of the container created.
      */
-    public String createContainer(String containerName){
+    public String createContainer(String containerName) {
         return null;
     }
 
     /**
      * Creates a container.
      *
-     * @param container
-     *      {@link CloudBlobContainer} object.
-     * @return
-     *      Name of the container.
+     * @param container {@link CloudBlobContainer} object.
+     * @return Name of the container.
      */
-    public String createContainer(CloudBlobContainer container){
+    public String createContainer(CloudBlobContainer container) {
         return null;
     }
 
     /**
      * Checks if a containers exists with the name given.
      *
-     * @param containerName
-     *      Name of the container.
+     * @param containerName Name of the container.
      * @return <code>false</code> if container does not exist, <code>true</code> otherwise.
      */
-    public boolean containerExists(String containerName){
+    public boolean containerExists(String containerName) {
         return false;
     }
 
     /**
      * Checks if a containers exists with the name given.
      *
-     * @param container
-     *      {@link CloudBlobContainer} object.
+     * @param container {@link CloudBlobContainer} object.
      * @return <code>false</code> if container does not exist, <code>true</code> otherwise.
      */
-    public boolean containerExists(CloudBlobContainer container){
+    public boolean containerExists(CloudBlobContainer container) {
         return false;
     }
 }
