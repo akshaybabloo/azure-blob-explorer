62f2e8d952e854239717fe3ecba513919927392d
Akshay Raj Gollahalli <akshay@gollahalli.com>
2018-06-21 09:51:55 +1200

    methods init

--- src/main/java/com/gollahalli/azure/AzureBlobDownload.java
+++ src/main/java/com/gollahalli/azure/AzureBlobDownload.java
@@ -23,21 +23,66 @@
  */
 package com.gollahalli.azure;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Implements downloading contents from Azure blob containers.
+ */
 public class AzureBlobDownload {
 
-    private String accountKey;
     private String accountName;
+    private String accountKey;
+    private String containerName;
+    private boolean useHttps;
+    private String saveTo;
 
-    public AzureBlobDownload(String accountKey, String accountName) {
-        this.accountKey = accountKey;
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    /**
+     * Implements downloading contents from Azure blob containers.
+     *
+     * @param accountName   Account name from your <strong>Access Keys</strong>.
+     * @param accountKey    Account key from your <strong>Access Keys</strong>.
+     * @param containerName Container name you want to uploadFromFile or create.
+     * @param useHttps      <code>true</code> to use HTTPS to connect to the storage service endpoints;
+     *                      otherwise, <code>false</code>. Defaults to <code>true</code>.
+     */
+    public AzureBlobDownload(String accountName, String accountKey, String containerName, boolean useHttps) {
         this.accountName = accountName;
+        this.accountKey = accountKey;
+        this.containerName = containerName;
+        this.useHttps = useHttps;
     }
 
-    public void downloadFile(String fileName){
+    /**
+     * Implements downloading contents from Azure blob containers with HTTPS as <code>true</code> by default.
+     *
+     * @param accountName   Account name from your <strong>Access Keys</strong>.
+     * @param accountKey    Account key from your <strong>Access Keys</strong>.
+     * @param containerName Container name you want to uploadFromFile or create.
+     */
+    public AzureBlobDownload(String accountName, String accountKey, String containerName) {
+        this(accountName, accountKey, containerName, true);
+    }
+
+    /**
+     * Download a file from a given blob path of the container.
+     *
+     * @param blobPathFileName Path to the file name on the container.
+     * @param saveTo Absolute path to a location on your computer.
+     */
+    public void downloadFile(String blobPathFileName, String saveTo) {
 
     }
 
-    public void downloadFolder(String folderName){
+    /**
+     * Download a blob folder and its contents.
+     *
+     * @param blobFolderPath Folder path on the container to download.
+     * @param saveTo Absolute path to a location on your computer.
+     */
+    public void downloadFolder(String blobFolderPath, String saveTo) {
 
     }
 }
