c806702b0d53cade119347a52922178f8eb09f3d
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2018-07-03 18:22:30 +1200

    overloaded constructor with HTTPS as true added

--- src/main/java/com/gollahalli/azure/AzureBlobStream.java
+++ src/main/java/com/gollahalli/azure/AzureBlobStream.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2018 Akshay Raj Gollahalli
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -62,6 +62,17 @@ public class AzureBlobStream {
     private boolean useHttps;
 
     private static final Logger LOGGER = LogManager.getLogger(AzureBlobDownload.class.getName());
+
+    /**
+     * Implements streaming contents from Azure blob containers with HTTPS.
+     *
+     * @param accountName   Account name from your <strong>Access Keys</strong>.
+     * @param accountKey    Account key from your <strong>Access Keys</strong>.
+     * @param containerName Container name you want to uploadFromFile or create.
+     */
+    public AzureBlobStream(String accountName, String accountKey, String containerName) {
+        this(accountName, accountKey, containerName, true);
+    }
 
     /**
      * Implements streaming contents from Azure blob containers.
